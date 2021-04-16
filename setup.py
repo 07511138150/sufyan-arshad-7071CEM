19163b255585c63a5c562a32565e6b16dd6b90d1
07511138150 <74307877+07511138150@users.noreply.github.com>
2021-04-16 18:39:24 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Web-Crawler',
+    version = '1.0',
+    packages = [],
+    url = '',
+    license='',
+    author='sufyan arshad',
+    author_email = '',
+    description = '',
+    install_requires = ['bs4', 'nltk', 'scikit-learn', 'numpy']
+)
